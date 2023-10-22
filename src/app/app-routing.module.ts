1b5da33912f1cf4d06b296eb54a775a947d6d977
Marcelo Vergara <marcelorv@gmail.com>
2023-10-22 08:42:28 -0300

    can deactivate route condition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRouters: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +21,7 @@ const appRouters: Routes = [
         component: ServersComponent,
         children: [
             { path: ':id', component: ServerComponent },
-            { path: ':id/edit', component: EditServerComponent },
+            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
   ] },
   { path: 'not-found', component: PageNotFoundComponent},
   { path: '**', redirectTo: '/not-found'},
